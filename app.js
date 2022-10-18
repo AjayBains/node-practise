f06ba344a5c304e481892366d50fc93156ab9bed
Ajay_Bains <ajay.bains@codvo.ai>
2022-10-18 12:38:57 +0530

    PORT variable added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(errorHandlerMiddleware);
 // app.patch('api/v1/tasks/:id') -update task
 // app.delete('api/v1/tasks/:id') -delete task
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
